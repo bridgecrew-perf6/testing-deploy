84bcc11e0c82ecebfd6c9809cd7107fe7092dd08
rizkyian78 <rizkyian78@gmail.com>
2022-06-22 19:42:03 +0700

    asda

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require("dotenv").config()
 
 module.exports = {
   "development": {
-    "username": process.env.USERNAME,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
+    "username": "rizkyian78",
+    "password": "Rizkyian_78",
+    "database": "database_development",
     "host": "127.0.0.1",
     "dialect": "mysql",
   },
